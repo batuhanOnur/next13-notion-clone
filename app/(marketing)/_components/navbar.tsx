c65527df4b75b58c0848a3e09db8917fdefc6ecc
batuhanonur <batuhanonur29@gmail.com>
2023-10-13 15:39:09 +0300

    auth & database

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,12 +1,19 @@
 "use client"
 
 import { useScrollTop } from "@/hooks/use-scroll-top"
+import Link from "next/link";
 import { cn } from "@/lib/utils"
 import { Logo } from './logo'
 import { ModeToggle } from "@/components/mode-toggle"
+import { useConvexAuth } from 'convex/react';
+import { SignInButton,UserButton } from '@clerk/clerk-react'
+import { Button } from "@/components/ui/button"
+import { Spinner } from "@/components/spinner"
+
 
 export const Navbar = () => {
 
+    const { isAuthenticated, isLoading } = useConvexAuth()
     const scrolled = useScrollTop()
 
     return (
@@ -17,6 +24,43 @@ export const Navbar = () => {
             <Logo />
 
             <div className="md-ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {
+                    isLoading && (
+                        <Spinner />
+                    )
+                }
+
+                {
+                    !isAuthenticated && !isLoading && (
+                        <>
+                            <SignInButton mode="modal">
+                                <Button variant="ghost" size="sm">
+                                    Login
+                                </Button>
+                            </SignInButton>
+
+                            <SignInButton mode="modal">
+                                <Button size="sm">
+                                    Get Jotion Free
+                                </Button>
+                            </SignInButton>
+                        </>
+                    )
+                }
+
+                {
+                    isAuthenticated && !isLoading && (
+                        <>
+                            <Button variant="ghost" size="sm">
+                                <Link href="/documents">
+                                    Enter Jotion
+                                </Link>
+                            </Button>
+
+                            <UserButton afterSignOutUrl="/"/>
+                        </>
+                    )
+                }
                 <ModeToggle />
             </div>
         </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers:[
+        {
+            domain: "https://busy-joey-62.clerk.accounts.dev",
+            applicationID: "convex"
+        }
+    ]
+}
