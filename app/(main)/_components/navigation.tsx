e058d9d3f54ebc5249d3ded128e9c210c8965f2d
batuhanonur <batuhanonur29@gmail.com>
2023-10-16 14:01:33 +0300

    sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation"
 import { useState,useEffect,useRef,ElementRef } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import { cn } from "@/lib/utils"
+import { UserItem } from "./user-item"
 
 export const Navigation = () => {
 
@@ -108,7 +109,7 @@ export const Navigation = () => {
             <aside
                 ref={sidebarRef}
                 className={cn(
-                    "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[999999]",
+                    "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
                     isResetting && "transition-all ease-in-out duration-300",
                     isMobile && "w-0"
                 )}
@@ -124,7 +125,7 @@ export const Navigation = () => {
                     <ChevronsLeft className="h-6 w-6"/>
                 </div>
                 <div>
-                    <p>Action Items</p>
+                    <UserItem />
                 </div>
                 <div className="mt-4">
                     <p>Documents</p>
@@ -140,7 +141,7 @@ export const Navigation = () => {
             <div
                 ref={navbarRef}
                 className={cn(
-                    "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
+                    "absolute top-0 z-[9999] left-60 w-[calc(100%-240px)]",
                     isResetting && "transition-all ease-in-out duration-300",
                     isMobile && "left-0 w-full"
                 )}
