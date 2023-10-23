fdaf2f5a7bca0c9d252559c97f151b1f4bd4bb1e
batuhanonur <batuhanonur29@gmail.com>
2023-10-23 10:06:15 +0300

    title & banner

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
+import { Banner } from "./banner";
 
 
 interface NavbarProps {
@@ -24,7 +25,11 @@ export const Navbar = ({
     })
 
     if(document === undefined) {
-        return <p>loading...</p>
+        return (
+            <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+                <Title.Skeleton />
+            </nav>
+        )
     }
 
     if(document === null) {
@@ -47,8 +52,16 @@ export const Navbar = ({
             <div className="flex items-center justify-between w-full">
                 <Title initialData={document}/>
             </div>
-
         </nav>
+
+        {
+            document.isArchive && (
+                <Banner 
+                    documentId={document._id}
+                    
+                />
+            )
+        }
        </>
     )
 }
