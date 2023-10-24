d4125243778d6f5b070d7efb0c4264d84bb7aa72
batuhanonur <batuhanonur29@gmail.com>
2023-10-24 13:40:52 +0300

    router

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -57,7 +57,8 @@ export const Item = ({
 
         if(!id) return;
 
-        const promise = archive({id})
+        const promise = archive({ id })
+        .then(() => router.push("/documents"))
 
         toast.promise(promise, {
             loading: "Moving to trash...",
@@ -88,8 +89,7 @@ export const Item = ({
                     onExpand?.()
                 }
 
-                //TODO: DONT FORGET
-                //router.push(`/documents/${documentId}`)
+                router.push(`/documents/${documentId}`);
             })
 
             toast.promise(promise, {
